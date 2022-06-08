9dde03079a63a4d934148aaa9b7f529863e10033
Diêgo Ferreira <56923620+diegofrr@users.noreply.github.com>
2022-06-07 22:42:29 -0300

    delete

--- src/components/BotaoAddComentario/index.js
+++
@@ -1,7 +0,0 @@
-import './addComentarioBtn.css'
-
-export default function BotaoAddComentario(props) {
-    return(
-        <button onClick={props.addComentario} className="addComentarioBtn">+</button>
-    );
-}
